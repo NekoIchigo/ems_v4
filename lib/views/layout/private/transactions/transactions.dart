9638ea2ce20d9c97f416ddd050249f7ff110eab6
Reydan <dexter.castillo@gets.ph-at-284246452697>
2024-01-03 18:21:33 +0800

    feat: mobile initials transactions

--- lib/views/layout/private/transactions/transactions.dart
+++ lib/views/layout/private/transactions/transactions.dart
@@ -1,5 +1,7 @@
+import 'package:ems_v4/global/constants.dart';
 import 'package:ems_v4/views/widgets/builder/ems_container.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 
 class Transactions extends StatefulWidget {
@@ -19,10 +21,65 @@ class _TransactionsState extends State<Transactions> {
         children: [
           SizedBox(
             height: Get.height * .78,
-            child: const Center(
+            child: Center(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [],
+                children: [
+                  const SizedBox(height: 10),
+                  const Text(
+                    "Transactions",
+                    style: TextStyle(
+                      color: primaryBlue,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                  SizedBox(
+                    height: Get.height * .6,
+                    child: GridView.count(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 40,
+                      ),
+                      mainAxisSpacing: 40,
+                      crossAxisSpacing: 40,
+                      crossAxisCount: 2,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            shadowColor: Colors.black,
+                            backgroundColor: bgSky,
+                            elevation: 10,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          child: const Icon(
+                            Icons.calendar_month,
+                            size: 80,
+                            color: bgPrimaryBlue,
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            shadowColor: Colors.black,
+                            backgroundColor: bgSky,
+                            elevation: 10,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          child: const Icon(
+                            Icons.edit_calendar_rounded,
+                            size: 80,
+                            color: bgPrimaryBlue,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
               ),
             ),
           ),
