72ef044187840e5e06a0649b5006dde2ac11d97e
Reydan <dexter.castillo@gets.ph-at-284246452697>
2024-01-03 11:38:39 +0800

    transactions_container initial

---
+++ lib/views/layout/private/transactions/transactions_container.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TransactionsContainer extends StatefulWidget {
+  const TransactionsContainer({super.key});
+
+  @override
+  State<TransactionsContainer> createState() => _TransactionsContainerState();
+}
+
+class _TransactionsContainerState extends State<TransactionsContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
