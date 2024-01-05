b67888af0ccb696da08cb63ee47277324a219fd3
Reydan <dexter.castillo@gets.ph-at-284246452697>
2024-01-05 10:41:20 +0800

    fix: use of model in authetication

--- lib/global/services/auth_service.dart
+++ lib/global/services/auth_service.dart
@@ -1,9 +1,9 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:ems_v4/global/api.dart';
 import 'package:ems_v4/models/company.dart';
 import 'package:ems_v4/models/employee.dart';
-import 'package:ems_v4/models/user.dart';
 import 'package:ems_v4/views/widgets/dialog/ems_dialog.dart';
 import 'package:ems_v4/views/widgets/dialog/get_dialog.dart';
 import 'package:flutter/material.dart';
@@ -11,20 +11,18 @@ import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthService extends GetxService {
-  // static AuthService get instance => Get.find();
-  late SharedPreferences localStorage;
+  late SharedPreferences _localStorage;
   final ApiCall apiCall = ApiCall();
 
   RxBool isLoading = false.obs;
   RxBool autheticated = false.obs;
   String? token;
-  late Rx<User> user;
   late Rx<Company> company;
   late Rx<Employee> employee;
 
   Future<AuthService> init() async {
-    localStorage = await SharedPreferences.getInstance();
-    token = localStorage.getString('token');
+    _localStorage = await SharedPreferences.getInstance();
+    token = _localStorage.getString('token');
     if (token != null) {
       var response = await apiCall.getRequest('/check-token');
       var result = jsonDecode(response.body);
@@ -39,70 +37,37 @@ class AuthService extends GetxService {
   }
 
   Future<void> setAuthStatus() async {
-    String? userData = localStorage.getString('user');
+    String? userData = _localStorage.getString('user');
     if (userData != null) {
       var data = jsonDecode(userData);
       var employeeData = data['employee'];
       var companyData = employeeData['company'];
 
-      user = User(
-        id: data['id'],
-        email: data['email'],
-        employeeNumber: data['employee_number'],
-      ).obs;
-      company = Company(id: companyData['id'], name: companyData['name']).obs;
-      employee = Employee(
-        id: employeeData['id'],
-        employeeNumber: employeeData['employee_details']['employee_number'],
-        firstName: employeeData['first_name'],
-        middleName: employeeData['middle_name'] ?? '',
-        lastName: employeeData['last_name'],
-        birthday: employeeData['birthday'],
-        gender: employeeData['gender'],
-        civilStatus: employeeData['civil_status'],
-      ).obs;
+      employee = Employee.fromJson(employeeData).obs;
+      company = Company.fromJson(companyData).obs;
     }
   }
 
   Future login(String email, String password) async {
     isLoading.value = true;
     try {
-      var response = await apiCall.postRequest(
-        {
-          'email': email,
-          'password': password,
-        },
-        '/mobile/login',
-      );
-
-      var result = jsonDecode(response.body);
+      final response = await apiCall
+          .postRequest({'email': email, 'password': password}, '/login');
+      final result = jsonDecode(response.body);
 
       if (result['success']) {
         autheticated.value = result['success'];
 
-        var data = result['data'];
-        var employeeData = data['employee'];
+        var userData = result['data'];
+        var employeeData = userData['employee'];
         var companyData = employeeData['company'];
 
-        localStorage.setString('token', result['token']);
-        localStorage.setString('user', jsonEncode(result['data']));
+        employee = Employee.fromJson(employeeData).obs;
+        company = Company.fromJson(companyData).obs;
 
-        user = User(
-          id: data['id'],
-          email: data['email'],
-          employeeNumber: data['employee_number'],
-        ).obs;
-        company = Company(id: companyData['id'], name: companyData['name']).obs;
-        employee = Employee(
-          id: employeeData['id'],
-          employeeNumber: employeeData['employee_details']['employee_number'],
-          firstName: employeeData['first_name'],
-          middleName: employeeData['middle_name'] ?? '',
-          lastName: employeeData['last_name'],
-          birthday: employeeData['birthday'],
-          gender: employeeData['gender'],
-          civilStatus: employeeData['civil_status'],
-        ).obs;
+        _localStorage.setString('token', result['token']);
+        _localStorage.setString('user', jsonEncode(result['data']));
+
         Get.offNamed('/');
       } else {
         Get.dialog(const GetDialog(
@@ -133,8 +98,8 @@ class AuthService extends GetxService {
 
   Future logout(BuildContext context) async {
     try {
-      apiCall.postRequest({}, '/mobile/logout').then((value) {
-        localStorage.clear();
+      apiCall.postRequest({}, '/logout').then((value) {
+        _localStorage.clear();
         Get.toNamed('/login');
       });
     } catch (error) {
