2b429f0766a4ddf25064a69d64a43e212934eaf9
Reydan <dexter.castillo@gets.ph-at-284246452697>
2024-01-09 14:19:47 +0800

    feat: adding back button notificaiton

--- lib/views/layout/private/main_navigation.dart
+++ lib/views/layout/private/main_navigation.dart
@@ -15,42 +15,83 @@ class MainNavigation extends StatefulWidget {
 
 class _MainNavigationState extends State<MainNavigation> {
   final MainNavigationController _mainNavigationController =
-      MainNavigationController();
+      Get.find<MainNavigationController>();
+  bool isBack = false;
+
+  Future<bool> exitDialog() async {
+    return false;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      resizeToAvoidBottomInset: false,
-      body: Stack(
-        children: [
-          const GettingStarted(),
-          Navigator(
-            key: Get.nestedKey(_mainNavigationController.routerKey),
-            onGenerateRoute: (settings) {
-              return GetPageRoute(
-                page: () => Obx(
-                  () => _mainNavigationController
-                      .pages[_mainNavigationController.selectedIndex.value],
-                ),
-                curve: Curves.easeInOut,
-              );
-            },
-          ),
-        ],
+    return PopScope(
+      canPop: false,
+      onPopInvoked: (bool didPop) async {
+        if (didPop) {
+          return;
+        }
+        final bool? shouldPop = await showExitConfirmationDialog(context);
+        if (shouldPop ?? false) {
+          if (mounted) Navigator.of(context).pop();
+        }
+      },
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: [
+            const GettingStarted(),
+            Navigator(
+              key: Get.nestedKey(_mainNavigationController.routerKey),
+              onGenerateRoute: (settings) {
+                return GetPageRoute(
+                  page: () => Obx(
+                    () => _mainNavigationController
+                        .pages[_mainNavigationController.selectedIndex.value],
+                  ),
+                  curve: Curves.easeInOut,
+                );
+              },
+            ),
+          ],
+        ),
+        extendBody: true,
+        bottomNavigationBar: ConvexAppBar(
+          backgroundColor: bgPrimaryBlue,
+          height: 55,
+          items: _mainNavigationController.navigations,
+          curveSize: 80,
+          top: -15,
+          style: TabStyle.reactCircle,
+          onTap: (index) {
+            _mainNavigationController.selectedIndex.value = index;
+          },
+        ),
       ),
-      extendBody: true,
-      bottomNavigationBar: ConvexAppBar(
-        backgroundColor: bgPrimaryBlue,
-        height: 55,
-        items: _mainNavigationController.navigations,
-        curveSize: 80,
-        top: -15,
-        style: TabStyle.reactCircle,
-        onTap: (index) {
-          _mainNavigationController.selectedIndex.value = index;
-        },
-      ),
+    );
+  }
+
+  Future<bool?> showExitConfirmationDialog(BuildContext context) async {
+    return showDialog<bool>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Confirm Exit'),
+          content: Text('Do you want to exit the app?'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () =>
+                  Navigator.of(context).pop(false), // User doesn't want to exit
+              child: Text('No'),
+            ),
+            TextButton(
+              onPressed: () =>
+                  Navigator.of(context).pop(true), // User wants to exit
+              child: Text('Yes'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
