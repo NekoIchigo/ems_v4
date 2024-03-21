bf4f9c144651e355a54c607e0b469c252985ca98
Reydan <dexter.castillo@gets.ph-at-284246452697>
2024-03-21 14:58:54 +0800

    home controller error handling

--- lib/global/controller/home_controller.dart
+++ lib/global/controller/home_controller.dart
@@ -1,10 +1,9 @@
-import 'dart:convert';
-
 import 'package:ems_v4/global/controller/auth_controller.dart';
 import 'package:ems_v4/global/controller/time_entries_controller.dart';
 import 'package:ems_v4/global/api.dart';
 import 'package:ems_v4/global/utils/date_time_utils.dart';
 import 'package:ems_v4/models/attendance_record.dart';
+import 'package:ems_v4/router/router.dart';
 import 'package:ems_v4/views/layout/private/home/widgets/health_declaration.dart';
 import 'package:ems_v4/views/layout/private/home/widgets/in_out_page.dart';
 import 'package:ems_v4/views/layout/private/home/widgets/information.dart';
@@ -21,8 +20,7 @@ class HomeController extends GetxController {
 
   final ApiCall apiCall = ApiCall();
   final DateTimeUtils dateTimeUtils = DateTimeUtils();
-  Rx<DateTime> workStart = DateTime.now().obs;
-  Rx<DateTime> workEnd = DateTime.now().obs;
+  Rx<DateTime> workStart = DateTime.now().obs, workEnd = DateTime.now().obs;
 
   final int routerKey = 1;
   RxInt pageIndex = 0.obs;
@@ -46,29 +44,30 @@ class HomeController extends GetxController {
 
   Future checkNewShift() async {
     isLoading.value = true;
-    try {
-      var response = await apiCall.getRequest('/check-shift');
-      var result = jsonDecode(response.body);
 
-      if (result.containsKey('success') && result['success']) {
-        var data = result['data'];
-        isNewShift.value = data['is_new_shift'];
-        isClockInOutComplete.value = data['is_shift_complete'];
-        isClockOut.value = data['is_clockout'];
-        workStart.value =
-            DateTime.parse("2024-01-01 ${data['schedule']['work_start']}");
-        workEnd.value =
-            DateTime.parse("2024-01-01 ${data['schedule']['work_end']}");
+    var result = await apiCall.getRequest(apiUrl: '/check-shift');
 
-        if (data['current_attendance_record'] != null) {
-          attendance =
-              AttendanceRecord.fromJson(data['current_attendance_record']).obs;
-        } else {
-          attendance = AttendanceRecord().obs;
-        }
+    if (result.containsKey('success') && result['success']) {
+      var data = result['data'];
+      isNewShift.value = data['is_new_shift'];
+      isClockInOutComplete.value = data['is_shift_complete'];
+      isClockOut.value = data['is_clockout'];
+      workStart.value =
+          DateTime.parse("2024-01-01 ${data['schedule']['work_start']}");
+      workEnd.value =
+          DateTime.parse("2024-01-01 ${data['schedule']['work_end']}");
+
+      if (data['current_attendance_record'] != null) {
+        attendance =
+            AttendanceRecord.fromJson(data['current_attendance_record']).obs;
       } else {
-        Get.dialog(
-          GemsDialog(
+        attendance = AttendanceRecord().obs;
+      }
+    } else {
+      showDialog(
+        context: navigatorKey.currentContext!,
+        builder: (context) {
+          return GemsDialog(
             title: "Oops",
             hasMessage: true,
             withCloseButton: true,
@@ -76,45 +75,43 @@ class HomeController extends GetxController {
             message: "Error Check Shift: ${result['message']}",
             type: "error",
             buttonNumber: 0,
-          ),
-        );
-      }
-    } catch (error) {
-      printError(info: 'Check New Shift Error: $error');
-    } finally {
-      isLoading.value = false;
+          );
+        },
+      );
     }
+
+    isLoading.value = false;
   }
 
   Future setClockInLocation() async {
     isLoading.value = true;
-    try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high);
-      var response = await apiCall.postRequest(
-        {
-          'latitude': position.latitude,
-          'longitude': position.longitude,
-        },
-        '/calculate-location/${_authService.employee!.value.id}',
-      );
 
-      var result = jsonDecode(response.body);
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    var result = await apiCall.postRequest(
+      data: {
+        'latitude': position.latitude,
+        'longitude': position.longitude,
+      },
+      apiUrl: '/calculate-location',
+    );
 
-      if (result.containsKey('success') && result['success']) {
-        var data = result['data'];
-        isInsideVicinity.value = data['is_inside_vicinity'];
-        currentLocation.value =
-            '${data['distance_in_km']} km away from designated office!';
+    if (result.containsKey('success') && result['success']) {
+      var data = result['data'];
+      isInsideVicinity.value = data['is_inside_vicinity'];
+      currentLocation.value =
+          '${data['distance_in_km']} km away from designated office!';
 
-        attendance.value.clockedInLocation = currentLocation.value;
-        attendance.value.clockedInLatitude = position.latitude.toString();
-        attendance.value.clockedInLongitude = position.longitude.toString();
-        attendance.value.clockedInLocationType =
-            isInsideVicinity.isTrue ? 'Within Vicinity' : 'Outside Vicinity';
-      } else {
-        Get.dialog(
-          const GemsDialog(
+      attendance.value.clockedInLocation = currentLocation.value;
+      attendance.value.clockedInLatitude = position.latitude.toString();
+      attendance.value.clockedInLongitude = position.longitude.toString();
+      attendance.value.clockedInLocationType =
+          isInsideVicinity.isTrue ? 'Within Vicinity' : 'Outside Vicinity';
+    } else {
+      showDialog(
+        context: navigatorKey.currentContext!,
+        builder: (context) {
+          return const GemsDialog(
             title: "Oops",
             hasMessage: true,
             withCloseButton: true,
@@ -122,52 +119,41 @@ class HomeController extends GetxController {
             message: "Error: Distance calculation unsuccessful",
             type: "error",
             buttonNumber: 0,
-          ),
-        );
-      }
-    } catch (error) {
-      Get.dialog(
-        GemsDialog(
-          title: "Oops",
-          hasMessage: true,
-          withCloseButton: true,
-          hasCustomWidget: false,
-          message: "Error: Unable to get your location \n Code: $error",
-          type: "error",
-          buttonNumber: 0,
-        ),
+          );
+        },
       );
-    } finally {
-      isLoading.value = false;
     }
+
+    isLoading.value = false;
   }
 
   Future setClockOutLocation() async {
     isLoading.value = true;
-    try {
-      Position position = await Geolocator.getCurrentPosition(
-          desiredAccuracy: LocationAccuracy.high);
-      var response = await apiCall.postRequest(
-        {
-          'latitude': position.latitude,
-          'longitude': position.longitude,
-        },
-        '/calculate-location/${_authService.employee!.value.id}',
-      );
-      var result = jsonDecode(response.body);
-      if (result.containsKey('success') && result['success']) {
-        var data = result['data'];
-        isInsideVicinity.value = data['is_inside_vicinity'];
-        currentLocation.value =
-            '${data['distance_in_km']} km away from designated office!';
-        attendance.value.clockedOutLocation = currentLocation.value;
-        attendance.value.clockedOutLatitude = position.latitude.toString();
-        attendance.value.clockedOutLongitude = position.longitude.toString();
-        attendance.value.clockedOutLocationType =
-            isInsideVicinity.isTrue ? 'Within Vicinity' : 'Outside Vicinity';
-      } else {
-        Get.dialog(
-          const GemsDialog(
+
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    var result = await apiCall.postRequest(
+      data: {
+        'latitude': position.latitude,
+        'longitude': position.longitude,
+      },
+      apiUrl: '/calculate-location',
+    );
+    if (result.containsKey('success') && result['success']) {
+      var data = result['data'];
+      isInsideVicinity.value = data['is_inside_vicinity'];
+      currentLocation.value =
+          '${data['distance_in_km']} km away from designated office!';
+      attendance.value.clockedOutLocation = currentLocation.value;
+      attendance.value.clockedOutLatitude = position.latitude.toString();
+      attendance.value.clockedOutLongitude = position.longitude.toString();
+      attendance.value.clockedOutLocationType =
+          isInsideVicinity.isTrue ? 'Within Vicinity' : 'Outside Vicinity';
+    } else {
+      showDialog(
+        context: navigatorKey.currentContext!,
+        builder: (context) {
+          return const GemsDialog(
             title: "Oops",
             hasMessage: true,
             withCloseButton: true,
@@ -175,24 +161,12 @@ class HomeController extends GetxController {
             message: "Error: Distance calculation unsuccessful",
             type: "error",
             buttonNumber: 0,
-          ),
-        );
-      }
-    } catch (error) {
-      Get.dialog(
-        GemsDialog(
-          title: "Oops",
-          hasMessage: true,
-          withCloseButton: true,
-          hasCustomWidget: false,
-          message: "Error:  Unable to get your location \n Code: $error",
-          type: "error",
-          buttonNumber: 0,
-        ),
+          );
+        },
       );
-    } finally {
-      isLoading.value = false;
     }
+
+    isLoading.value = false;
   }
 
   Future clockIn({
@@ -202,18 +176,19 @@ class HomeController extends GetxController {
     isLoading.value = true;
     String healthCheckStr = healthCheck.join(', ');
     double userTemperature = 37.0;
+
     if (temperature != null) {
       userTemperature = double.parse(temperature);
     }
+
     if (healthCheck.isNotEmpty || userTemperature >= 37.8) {
       isUserSick.value = true;
     } else {
       isUserSick.value = false;
     }
 
-    try {
-      // print('called');
-      var response = await apiCall.postRequest({
+    var result = await apiCall.postRequest(
+      data: {
         'employee_id': _authService.employee!.value.id,
         'clocked_in_location': attendance.value.clockedInLocation,
         'clocked_in_latitude': attendance.value.clockedInLatitude,
@@ -223,147 +198,69 @@ class HomeController extends GetxController {
             attendance.value.clockedInLocationSetting,
         'health_check': healthCheckStr,
         'health_temperature': temperature,
-      }, '/clock-in');
-      var result = jsonDecode(response.body);
-      if (result.containsKey('success') && result['success']) {
-        _timeEntriesController.getAttendanceList(
-            employeeId: _authService.employee!.value.id, days: 1);
-        checkNewShift();
-      } else {
-        Get.dialog(GemsDialog(
-          title: "Oops",
-          hasMessage: true,
-          withCloseButton: true,
-          hasCustomWidget: false,
-          message: "Error: $result",
-          type: "error",
-          buttonNumber: 0,
-        ));
-      }
-    } catch (error) {
-      Get.dialog(GemsDialog(
-        title: "Oops",
-        hasMessage: true,
-        withCloseButton: true,
-        hasCustomWidget: false,
-        message: "Error: $error",
-        type: "error",
-        buttonNumber: 0,
-      ));
-      printError(info: 'Error Message: $error');
-      isLoading.value = false;
-      pageName.value = '/home';
-    } finally {
-      isLoading.value = false;
+      },
+      apiUrl: '/clock-in',
+    );
+    if (result.containsKey('success') && result['success']) {
+      _timeEntriesController.getAttendanceList(
+          employeeId: _authService.employee!.value.id, days: 1);
+      checkNewShift();
+    } else {
+      showDialog(
+        context: navigatorKey.currentContext!,
+        builder: (context) {
+          return GemsDialog(
+            title: "Oops",
+            hasMessage: true,
+            withCloseButton: true,
+            hasCustomWidget: false,
+            message: "Error: $result",
+            type: "error",
+            buttonNumber: 0,
+          );
+        },
+      );
     }
+
+    isLoading.value = false;
   }
 
   Future clockOut({required BuildContext context}) async {
     isLoading.value = true;
-    try {
-      var response = await apiCall.postRequest({
-        'attendance_id': attendance.value.id,
-        'clocked_out_location': attendance.value.clockedOutLocation,
-        'clocked_out_latitude': attendance.value.clockedOutLatitude,
-        'clocked_out_longitude': attendance.value.clockedOutLongitude,
-        'clocked_out_location_type': attendance.value.clockedOutLocationType,
-        'clocked_out_location_setting':
-            attendance.value.clockedOutLocationSetting,
-      }, '/clock-out');
-      var result = jsonDecode(response.body);
-      if (result.containsKey('success') && result['success']) {
-        _timeEntriesController.getAttendanceList(
-            employeeId: _authService.employee!.value.id, days: 1);
-        checkNewShift();
-      } else {
-        Get.dialog(GemsDialog(
-          title: "Oops",
-          hasMessage: true,
-          withCloseButton: true,
-          hasCustomWidget: false,
-          message: "Error clock out result: $result",
-          type: "error",
-          buttonNumber: 0,
-        ));
 
-        pageName.value = '/home';
-      }
-    } catch (error) {
-      Get.dialog(
-        GemsDialog(
-          title: "Oops",
-          hasMessage: true,
-          withCloseButton: true,
-          hasCustomWidget: false,
-          message: "Error clockout: $error",
-          type: "error",
-          buttonNumber: 0,
-        ),
+    var result = await apiCall.postRequest(data: {
+      'attendance_id': attendance.value.id,
+      'clocked_out_location': attendance.value.clockedOutLocation,
+      'clocked_out_latitude': attendance.value.clockedOutLatitude,
+      'clocked_out_longitude': attendance.value.clockedOutLongitude,
+      'clocked_out_location_type': attendance.value.clockedOutLocationType,
+      'clocked_out_location_setting':
+          attendance.value.clockedOutLocationSetting,
+    }, apiUrl: '/clock-out');
+    if (result.containsKey('success') && result['success']) {
+      _timeEntriesController.getAttendanceList(
+          employeeId: _authService.employee!.value.id, days: 1);
+      checkNewShift();
+    } else {
+      showDialog(
+        context: navigatorKey.currentContext!,
+        builder: (context) {
+          return GemsDialog(
+            title: "Oops",
+            hasMessage: true,
+            withCloseButton: true,
+            hasCustomWidget: false,
+            message: "Error clock out result: $result",
+            type: "error",
+            buttonNumber: 0,
+          );
+        },
       );
-    } finally {
-      isLoading.value = false;
+
       pageName.value = '/home';
     }
-  }
 
-  Future additionalShiftClockin(String reason, String location) async {
-    isLoading.value = true;
-    try {
-      var response = await apiCall.postRequest({
-        'attendance_records_id': attendance.value.id,
-        'clocked_in_location': location,
-        'clocked_in_latitude': attendance.value.clockedInLatitude,
-        'clocked_in_longitude': attendance.value.clockedInLongitude,
-        'clocked_in_location_type': attendance.value.clockedInLocationType,
-        'clocked_out_location_setting': reason,
-      }, '/additional-shift-clockin');
-      var result = jsonDecode(response.body);
-      printInfo(info: result.toString());
-    } catch (error) {
-      Get.dialog(
-        GemsDialog(
-          title: "Oops",
-          hasMessage: true,
-          withCloseButton: true,
-          hasCustomWidget: false,
-          message: "Error clockout: $error",
-          type: "error",
-          buttonNumber: 0,
-        ),
-      );
-    } finally {
-      isLoading.value = false;
-    }
-  }
-
-  Future additionalShiftClockout(String reason, String location) async {
-    isLoading.value = true;
-    try {
-      var response = await apiCall.postRequest({
-        'attendance_records_id': attendance.value.id,
-        'clocked_in_location': location,
-        'clocked_in_latitude': attendance.value.clockedInLatitude,
-        'clocked_in_longitude': attendance.value.clockedInLongitude,
-        'clocked_in_location_type': attendance.value.clockedInLocationType,
-        'clocked_out_location_setting': reason,
-      }, '/additional-shift-clockout');
-      var result = jsonDecode(response.body);
-      printInfo(info: result.toString());
-    } catch (error) {
-      Get.dialog(
-        GemsDialog(
-          title: "Oops",
-          hasMessage: true,
-          withCloseButton: true,
-          hasCustomWidget: false,
-          message: "Error clockout: $error",
-          type: "error",
-          buttonNumber: 0,
-        ),
-      );
-    } finally {
-      isLoading.value = false;
-    }
+    isLoading.value = false;
   }
 
   String getWorkingHrs({DateTime? dateTimeIn, DateTime? dateTimeOut}) {
@@ -380,50 +277,4 @@ class HomeController extends GetxController {
       return "?? : ??";
     }
   }
-
-  // Future getLatestLog({required int employeeId}) async {
-  //   isLoading.value = true;
-  //   try {
-  //     var response = await apiCall.getRequest('/latest-dtr/$employeeId');
-  //     var result = jsonDecode(response.body);
-
-  //     if (result['success']) {
-  //       if (result['data'] != null) {
-  //         if (attendance.value.clockInAt != null &&
-  //             attendance.value.clockOutAt == null) {
-  //           // isClockOut.value = true;
-  //           // isClockInOutComplete.value = false;
-  //         } else {
-  //           // isClockOut.value = false;
-  //         }
-  //       }
-  //     } else {
-  //       Get.dialog(GemsDialog(
-  //         title: "Oops",
-  //         hasMessage: true,
-  //         withCloseButton: true,
-  //         hasCustomWidget: false,
-  //         message: "Error: $result",
-  //         type: "error",
-  //         buttonNumber: 0,
-  //       ));
-  //       pageName.value = '/home';
-  //       printError(info: 'Error Message getLatestLog: Invalid Request');
-  //     }
-  //   } catch (error) {
-  //     Get.dialog(GemsDialog(
-  //       title: "Oops",
-  //       hasMessage: true,
-  //       withCloseButton: true,
-  //       hasCustomWidget: false,
-  //       message: "Error: $error",
-  //       type: "error",
-  //       buttonNumber: 0,
-  //     ));
-  //     pageName.value = '/home';
-  //     printError(info: 'Error Message getLatestLog: $error');
-  //   } finally {
-  //     isLoading.value = false;
-  //   }
-  // }
 }
