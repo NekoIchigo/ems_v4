75bf13a8839113fefa6f14b0cd9ada2e90484cc7
Reydan <reydanjohnbelen@gmail.com>
2024-09-27 16:07:16 +0800

    trasactions notification

--- lib/global/controller/notification_controller.dart
+++ lib/global/controller/notification_controller.dart
@@ -1,11 +1,24 @@
+import 'dart:async';
+
 import 'package:ems_v4/global/api.dart';
 import 'package:ems_v4/global/controller/auth_controller.dart';
+import 'package:ems_v4/global/controller/change_schedule_controller.dart';
+import 'package:ems_v4/global/controller/leave_controller.dart';
+import 'package:ems_v4/global/controller/overtime_controller.dart';
+import 'package:ems_v4/models/transaction_item.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
 
 class NotificationController extends GetxController {
   final AuthController _authController = Get.find<AuthController>();
+  final LeaveController _leaveController = Get.find<LeaveController>();
+  final ChangeScheduleController _changeSchedule =
+      Get.find<ChangeScheduleController>();
+  final OvertimeController _overtime = Get.find<OvertimeController>();
   final ApiCall _apiCall = ApiCall();
   RxBool isLoading = false.obs,
+      isTransactionLoading = false.obs,
       isPaginateLoading = false.obs,
       showNotificationBadge = false.obs;
   RxList notificationList = [].obs;
@@ -26,6 +39,75 @@ class NotificationController extends GetxController {
     }).whenComplete(() {
       isLoading.value = false;
     });
+  }
+
+  Future showOvertime(int parentId, BuildContext context) async {
+    isTransactionLoading.value = true;
+    _apiCall
+        .getRequest(apiUrl: "/mobile/overtime/$parentId")
+        .then((response) async {
+      if (response.containsKey("success") && response["success"]) {
+        final data = response["data"];
+        final transactionItem = TransactionItem(
+          id: data['id'],
+          title: "",
+          dateCreated: "",
+          subtitle: "",
+          status: data['status'],
+          type: "",
+          data: data,
+        );
+        await _overtime.getLogs(parentId);
+        _overtime.transactionData = transactionItem.toMap().obs;
+        context.push('/overtime_form', extra: transactionItem.toMap());
+      }
+    }).whenComplete(() => isTransactionLoading.value = false);
+  }
+
+  Future showChangeSchedule(int parentId, BuildContext context) async {
+    isTransactionLoading.value = true;
+    _apiCall
+        .getRequest(apiUrl: "/mobile/change-schedule/$parentId")
+        .then((response) async {
+      if (response.containsKey("success") && response["success"]) {
+        final data = response["data"];
+        final transactionItem = TransactionItem(
+          id: data['id'],
+          title: "",
+          dateCreated: "",
+          subtitle: "",
+          status: data['status'],
+          type: "",
+          data: data,
+        );
+        await _changeSchedule.getLogs(parentId);
+        _changeSchedule.transactionData = transactionItem.toMap().obs;
+        context.push('/change_schedule_form', extra: transactionItem.toMap());
+      }
+    }).whenComplete(() => isTransactionLoading.value = false);
+  }
+
+  Future showLeaveRequest(int parentId, BuildContext context) async {
+    isTransactionLoading.value = true;
+    _apiCall
+        .getRequest(apiUrl: "/mobile/leave/$parentId")
+        .then((response) async {
+      if (response.containsKey("success") && response["success"]) {
+        final data = response["data"];
+        final transactionItem = TransactionItem(
+          id: data['id'],
+          title: "",
+          dateCreated: "",
+          subtitle: "",
+          status: data['status'],
+          type: "",
+          data: data,
+        );
+        await _leaveController.getLogs(parentId);
+        _leaveController.transactionData = transactionItem.toMap().obs;
+        context.push('/leave_form', extra: transactionItem.toMap());
+      }
+    }).whenComplete(() => isTransactionLoading.value = false);
   }
 
   Future nextPageList() async {}
