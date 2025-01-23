ff6960acf2524c36d44a43ecd56f64153b085bae
Reydan <reydanjohnbelen@gmail.com>
2025-01-23 14:33:45 +0800

    adjust announcement padding

--- lib/views/widgets/dialog/announcement_dialog.dart
+++ lib/views/widgets/dialog/announcement_dialog.dart
@@ -34,91 +34,88 @@ class _AnnouncementDialogState extends State<AnnouncementDialog> {
       insetAnimationDuration: const Duration(milliseconds: 100),
       child: Stack(
         children: [
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: FlutterCarousel(
-              options: FlutterCarouselOptions(
-                height: size.height * .75,
-                showIndicator: widget.items.length > 1,
-                enableInfiniteScroll: widget.items.length > 1,
-                slideIndicator: CircularSlideIndicator(),
-                autoPlay: widget.items.length > 1,
-                autoPlayAnimationDuration: const Duration(seconds: 3),
-              ),
-              items: widget.items.map((item) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: const EdgeInsets.symmetric(horizontal: 10.0),
-                      child: Column(
-                        children: [
-                          const SizedBox(height: 40),
-                          Visibility(
-                            visible: item['banner_path'] != null,
-                            child: SizedBox(
-                              height: 250,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(20.0),
-                                child: item['banner_path'] != null
-                                    ? File(item['banner_path']).existsSync()
-                                        ? Image.file(File(item['banner_path']),
-                                            fit: BoxFit.fitWidth)
-                                        : Image.asset(
-                                            'assets/images/announcement.png',
-                                            fit: BoxFit.fitWidth,
-                                          )
-                                    : Image.asset(
-                                        'assets/images/announcement.png',
-                                        fit: BoxFit.fitWidth,
-                                      ),
-                              ),
-                            ),
-                          ),
-                          Visibility(
-                            visible: item['banner_path'] == null,
-                            child: SizedBox(
-                              height: 250,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(20.0),
-                                child: Image.asset(
-                                  'assets/images/announcement.png',
-                                  fit: BoxFit.fitWidth,
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(height: 15),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(item['user']['name']),
-                              Text(_dateTimeUtils
-                                  .fromLaravelDateFormat(item['start_date']))
-                            ],
-                          ),
-                          const SizedBox(height: 15),
-                          Text(
-                            item['name'],
-                            style: titleStyle,
-                            textAlign: TextAlign.center,
-                          ),
-                          const SizedBox(height: 15),
-                          SizedBox(
-                            height: 200,
-                            child: SingleChildScrollView(
-                              child: HtmlWidget(
-                                item['content'],
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                );
-              }).toList(),
+          FlutterCarousel(
+            options: FlutterCarouselOptions(
+              height: size.height * .75,
+              showIndicator: widget.items.length > 1,
+              enableInfiniteScroll: widget.items.length > 1,
+              slideIndicator: CircularSlideIndicator(),
+              autoPlay: widget.items.length > 1,
+              autoPlayAnimationDuration: const Duration(seconds: 3),
             ),
+            items: widget.items.map((item) {
+              return Builder(
+                builder: (BuildContext context) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 40),
+                        Visibility(
+                          visible: item['banner_path'] != null,
+                          child: SizedBox(
+                            width: size.width,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: item['banner_path'] != null
+                                  ? File(item['banner_path']).existsSync()
+                                      ? Image.file(File(item['banner_path']),
+                                          fit: BoxFit.fitWidth)
+                                      : Image.asset(
+                                          'assets/images/announcement.png',
+                                          fit: BoxFit.fitWidth,
+                                        )
+                                  : Image.asset(
+                                      'assets/images/announcement.png',
+                                      fit: BoxFit.fitWidth,
+                                    ),
+                            ),
+                          ),
+                        ),
+                        Visibility(
+                          visible: item['banner_path'] == null,
+                          child: SizedBox(
+                            width: size.width,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(20.0),
+                              child: Image.asset(
+                                'assets/images/announcement.png',
+                                fit: BoxFit.fitWidth,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(item['user']['name']),
+                            Text(_dateTimeUtils
+                                .fromLaravelDateFormat(item['start_date']))
+                          ],
+                        ),
+                        const SizedBox(height: 15),
+                        Text(
+                          item['name'],
+                          style: titleStyle,
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 15),
+                        SizedBox(
+                          height: 200,
+                          child: SingleChildScrollView(
+                            child: HtmlWidget(
+                              item['content'],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            }).toList(),
           ),
           Positioned(
             right: 0,
